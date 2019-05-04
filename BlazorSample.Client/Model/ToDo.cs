5a5683a5dd56e498bc508d7cf205646e02f3ca84
Mike Lyncheski <mll@harriergroup.com>
2019-05-04 15:35:21 -0400

    Add Basic To Do List Functionality
    This sample is based loosely on the https://www.youtube.com/watch?v=bbHzyIsqGuU Microsoft Blazor - Getting Started video from DevExpress.

---
+++ BlazorSample.Client/Model/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSample.Client.Model
+{
+	public class ToDo
+	{
+		public string Title { get; set; }
+		public bool IsCompleted { get; set; }
+	}
+}
